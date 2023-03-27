ede91890d5908feae70db8e0b4a540dcd8e09f05
kobi <kobikahn@gmail.com>
2023-03-27 12:46:52 -0400

    Working on app

--- main.py
+++ main.py
@@ -84,11 +84,22 @@ def stand_dev(data):
     s = math.sqrt(v)
     return s
 # year_built
-def mini_df(df, zip_list, col_names):
-    big_dict = {}
-    for zip in zip_list:
-        stats_dict = {}
-        print(df.index[zip])
+
+
+# GET USER INPUT
+def user_input():
+    # print('WELCOME TO KOBI\'S HOUSE FINDER\nPLEAE ENTER THE FOLLOWING INFORMATION WITH NO SPACES AND PRESS ENTER TO EACH.')
+    # zip_entry = input('ENTER ZIPCODE HERE: ')
+    # price_entry = input('ENTER PRICE HERE: ')
+    # bath_entry = input('ENTER NUMBER OF BATHS HERE: ')
+    # bed_entry = input('ENTER NUMBER OF BEDS HERE: ')
+    # floor_entry = input('ENTER NUMBER OF FLOORS HERE: ')
+    # sqft_entry = input('ENTER SQARE FOOT LIVING SPACE HERE: ')
+    # print('THANK YOU! HERE ARE YOUR FIVE BEST OPTIONS...')
+    user_list = ['WA 98133', 342000, 3, 2.5, 3, 6200]
+    # user_list = [zip_entry, price_entry, bed_entry, bath_entry, floor_entry, sqft_entry]
+    return(user_list)
+
 
 
 
@@ -101,68 +112,106 @@ house_df = pd.read_csv('data.csv', delim_whitespace=False)
 house_df['price'] = house_df['price'].round()
 house_df = house_df[(house_df['price']>0) & (house_df['price']<5000000)]
 
-training_set, test_set = get_random(house_df, 50)
+# MAIN APP PROJECT
 
-training_set['sqft_living'] = training_set['sqft_living'].astype('float64')
-training_set['sqft_lot'] = training_set['sqft_lot'].astype('float64')
-# print(house_df)
-# print(house_df.iloc1[2])
-#ZIP CODE STUFF
-zip_df = house_df
-zip_dict = {}
-zip_list = []
-large_zip = []
-for zip in house_df['statezip']:
-    zip = zip.split()[-1]
-    zip_list.append(zip)
-    if zip in zip_dict.keys():
-        zip_dict[zip] += 1
-    else:
-        zip_dict[zip] = 0
-house_df = house_df.set_axis(zip_list)
+# NORMALIZE MAIN DATA
 
-for key, val in zip_dict.items():
-    if val > 95:
-        large_zip.append(key)
-bad_index = []
-for index in house_df.index:
-    if index not in large_zip:
-        bad_index.append(index)
+# house_df['price'] = (house_df.price - house_df.price.mean())/house_df.price.std()
+# house_df['bedrooms'] = (house_df.bedrooms - house_df.bedrooms.mean())/house_df.bedrooms.std()
+# house_df['bathrooms'] = (house_df.bathrooms - house_df.bathrooms.mean())/house_df.bathrooms.std()
+# house_df['floors'] = (house_df.floors - house_df.floors.mean())/house_df.floors.std()
+# house_df['sqft_living'] = (house_df.sqft_living - house_df.sqft_living.mean())/house_df.sqft_living.std()
 
-zip_df = house_df.drop(bad_index, inplace=False)
+price_list = house_df['price']
+bed_list = house_df['bedrooms']
+bath_list = house_df['bathrooms']
+floor_list = house_df['floors']
+sqft_list = house_df['sqft_living']
 
 
-mini_df(zip_df, large_zip, ['price', 'bathrooms', 'bedrooms', 'sqft_living', 'sqft_lot', 'yr_built'])
+user_list = user_input()
 
+price_list.append(user_list[1])
+bed_list.append(user_list[2])
+bath_list.append(user_list[3])
+floor_list.append(user_list[4])
+sqft_list.append(user_list[5])
 
-# FIND SLOPES
-x1, b1 = regression_equ(training_set['sqft_living'], training_set['price'])
-x2, b2 = regression_equ(training_set['sqft_lot'], training_set['price'])
-y1 = []
-y2 = []
-for item in training_set['sqft_living']:
-    y1.append(item * int(x1) + int(b1))
-for item in training_set['sqft_lot']:
-    y2.append(item * int(x2) + int(b2))
+price_list = (price_list - price_list.mean())/price_list.std()
+bed_list = (bed_list - bed_list.mean())/bed_list.std()
+bath_list = (bath_list - bath_list.mean())/bath_list.std()
+floor_list = (floor_list - floor_list.mean())/floor_list.std()
+sqft_list = (sqft_list - sqft_list.mean())/sqft_list.std()
 
-# MULTI LINEAR REGRESSION
-liv_x2_coeff, liv_x_coeff, liv_con_coeff = regression_equ(training_set['sqft_living'], training_set['price'], False)
-lot_x2_coeff, lot_x_coeff, lot_con_coeff = regression_equ(training_set['sqft_lot'], training_set['price'], False)
-
-new_liv_x, liv_y = scatter_quadratic(training_set['sqft_living'], liv_x2_coeff, liv_x_coeff, liv_con_coeff)
-new_lot_x, lot_y = scatter_quadratic(training_set['sqft_lot'], lot_x2_coeff, lot_x_coeff, lot_con_coeff)
-
-# GRAPH THE STUFF
-fig, ax = plt.subplots(nrows=1, ncols=4)
-training_set.plot.scatter('sqft_living', 'price', ax=ax[0])
-ax[0].plot(training_set['sqft_living'], y1)
-training_set.plot.scatter('sqft_lot', 'price', ax=ax[1])
-ax[1].plot(training_set['sqft_lot'], y2)
-training_set.plot.scatter('sqft_living', 'price', ax=ax[2])
-ax[2].plot(new_liv_x, liv_y)
-training_set.plot.scatter('sqft_lot', 'price', ax=ax[3])
-ax[3].plot(new_lot_x, lot_y)
-# plt.show()
+user_list = [user_list[0]]
+user_list.append(price_list[-1])
+user_list.append(bed_list[-1])
+user_list.append(bath_list[-1])
+user_list.append(floor_list[-1])
+user_list.append(sqft_list[-1])
+print(user_list)
 
 
 
+# training_set, test_set = get_random(house_df, 50)
+# training_set['sqft_living'] = training_set['sqft_living'].astype('float64')
+# training_set['sqft_lot'] = training_set['sqft_lot'].astype('float64')
+# # print(house_df)
+# # print(house_df.iloc1[2])
+# #ZIP CODE STUFF
+# zip_df = house_df
+# zip_dict = {}
+# zip_list = []
+# large_zip = []
+# for zip in house_df['statezip']:
+#     zip = zip.split()[-1]
+#     zip_list.append(zip)
+#     if zip in zip_dict.keys():
+#         zip_dict[zip] += 1
+#     else:
+#         zip_dict[zip] = 0
+# house_df = house_df.set_axis(zip_list)
+#
+# for key, val in zip_dict.items():
+#     if val > 95:
+#         large_zip.append(key)
+# bad_index = []
+# for index in house_df.index:
+#     if index not in large_zip:
+#         bad_index.append(index)
+#
+# zip_df = house_df.drop(bad_index, inplace=False)
+
+
+#
+# # FIND SLOPES
+# x1, b1 = regression_equ(training_set['sqft_living'], training_set['price'])
+# x2, b2 = regression_equ(training_set['sqft_lot'], training_set['price'])
+# y1 = []
+# y2 = []
+# for item in training_set['sqft_living']:
+#     y1.append(item * int(x1) + int(b1))
+# for item in training_set['sqft_lot']:
+#     y2.append(item * int(x2) + int(b2))
+#
+# # MULTI LINEAR REGRESSION
+# liv_x2_coeff, liv_x_coeff, liv_con_coeff = regression_equ(training_set['sqft_living'], training_set['price'], False)
+# lot_x2_coeff, lot_x_coeff, lot_con_coeff = regression_equ(training_set['sqft_lot'], training_set['price'], False)
+#
+# new_liv_x, liv_y = scatter_quadratic(training_set['sqft_living'], liv_x2_coeff, liv_x_coeff, liv_con_coeff)
+# new_lot_x, lot_y = scatter_quadratic(training_set['sqft_lot'], lot_x2_coeff, lot_x_coeff, lot_con_coeff)
+#
+# # GRAPH THE STUFF
+# fig, ax = plt.subplots(nrows=1, ncols=4)
+# training_set.plot.scatter('sqft_living', 'price', ax=ax[0])
+# ax[0].plot(training_set['sqft_living'], y1)
+# training_set.plot.scatter('sqft_lot', 'price', ax=ax[1])
+# ax[1].plot(training_set['sqft_lot'], y2)
+# training_set.plot.scatter('sqft_living', 'price', ax=ax[2])
+# ax[2].plot(new_liv_x, liv_y)
+# training_set.plot.scatter('sqft_lot', 'price', ax=ax[3])
+# ax[3].plot(new_lot_x, lot_y)
+# # plt.show()
+#
+#
+#
